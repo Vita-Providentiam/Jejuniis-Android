362d25b0f72e8a940b36a77e4f891bd98e03e5d0
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@keyidentity.com>
2018-05-18 16:04:13 +0200

    Animating FAB icon

--- app/src/main/java/org/providentiam/jejuniisdiebus/MainActivity.kt
+++ app/src/main/java/org/providentiam/jejuniisdiebus/MainActivity.kt
@@ -1,14 +1,21 @@
 package org.providentiam.jejuniisdiebus
 
 import android.content.Intent
+import android.graphics.drawable.Animatable2
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.annotation.RequiresApi
 import android.support.design.widget.NavigationView
+import android.support.graphics.drawable.Animatable2Compat
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -19,9 +26,37 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab_menu.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener { _ ->
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                @Suppress("CAST_NEVER_SUCCEEDS")
+                val anim = fab.drawable as AnimatedVectorDrawable
+                var callback: Animatable2.AnimationCallback? = null
+                callback = object : Animatable2.AnimationCallback() {
+
+                    @RequiresApi(Build.VERSION_CODES.M)
+                    override fun onAnimationEnd(drawable: Drawable?) {
+                        super.onAnimationEnd(drawable)
+                        fabAction()
+                        anim.unregisterAnimationCallback(callback!!)
+                    }
+                }
+                anim.registerAnimationCallback(callback)
+
+                anim.start()
+            } else {
+                val anim = fab.drawable as AnimatedVectorDrawableCompat
+                var callback: Animatable2Compat.AnimationCallback? = null
+                callback = object : Animatable2Compat.AnimationCallback() {
+                    override fun onAnimationEnd(drawable: Drawable?) {
+                        super.onAnimationEnd(drawable)
+                        fabAction()
+                        anim.unregisterAnimationCallback(callback!!)
+                    }
+                }
+                anim.registerAnimationCallback(callback)
+
+                anim.start()
+            }
         }
 
         val toggle = ActionBarDrawerToggle(
@@ -30,6 +65,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun fabAction() {
+        fab_menu.visibility = View.VISIBLE
+        fab.visibility = View.GONE
     }
 
     override fun onBackPressed() {
@@ -50,13 +90,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings ->{
                 val intent = Intent(this, SettingsActivity::class.java)
                 startActivity(intent)
-                return true
+                true
             }
-            else -> return super.onOptionsItemSelected(item)
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
