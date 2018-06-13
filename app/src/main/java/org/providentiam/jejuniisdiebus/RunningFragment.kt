258ec4dd0b216ca53f00988ed2da88b848883636
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@keyidentity.com>
2018-06-13 13:17:36 +0200

    Working chronometer

--- app/src/main/java/org/providentiam/jejuniisdiebus/RunningFragment.kt
+++ app/src/main/java/org/providentiam/jejuniisdiebus/RunningFragment.kt
@@ -7,13 +7,17 @@ import android.support.graphics.drawable.AnimatedVectorDrawableCompat
 import android.support.v4.app.Fragment
 import android.transition.Transition
 import android.transition.TransitionInflater
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.ViewSwitcher
 import com.github.clans.fab.FloatingActionMenu
 import kotlinx.android.synthetic.main.content_running.*
 import org.providentiam.jejuniisdiebus.utils.FabHelper
 import org.providentiam.jejuniisdiebus.utils.FabHelperApi23
+
 
 class RunningFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -22,6 +26,17 @@ class RunningFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val fasting_goal = 36 * 60L * 60L
+
+        goal_chronometer.setFactory(factory)
+
+        chronometer.start()
+        chronometer.setOnChronometerTickListener {
+            val value = fasting_goal - getSecondsFromDurationString(chronometer.text.toString())
+            val hours = value / 3600
+            val minutes =  (value - (hours * 3600)) / 60
+            goal_chronometer?.setText("$hours:$minutes to reach goal")
+        }
 
         fabClock.setOnClickListener { _ ->
             fabAction()
@@ -30,7 +45,8 @@ class RunningFragment : Fragment() {
 
     private fun fabAction() {
         val mainFragment = MainFragment()
-        val enterTransition =  TransitionInflater.from(this.activity).inflateTransition(R.transition.content_main_in)
+        val enterTransition = TransitionInflater.from(this.activity).inflateTransition(R.transition.content_main_in)
+        chronometer.stop()
 
         enterTransition.addListener(object : Transition.TransitionListener {
             override fun onTransitionEnd(transition: Transition?) {
@@ -72,6 +88,37 @@ class RunningFragment : Fragment() {
                         FabHelper.resetAnimation(fab, R.drawable.fasting_to_plus)
                     }
                 })
-        .commit()
+                .commit()
+    }
+
+    private fun getSecondsFromDurationString(value: String): Long {
+
+        val parts = value.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+        if (parts.size < 2 || parts.size > 3)
+            return 0
+
+        var seconds = 0
+        var minutes = 0
+        var hours = 0
+
+        if (parts.size == 2) {
+            seconds = Integer.parseInt(parts[1])
+            minutes = Integer.parseInt(parts[0])
+        } else if (parts.size == 3) {
+            seconds = Integer.parseInt(parts[2])
+            minutes = Integer.parseInt(parts[1])
+            hours = Integer.parseInt(parts[0])
+        }
+
+        return seconds + minutes * 60L + hours * 3600L
+    }
+
+    private val factory = ViewSwitcher.ViewFactory {
+        val t = TextView(activity)
+        t.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+        t.setTextAppearance(activity, android.R.style.TextAppearance_Small
+        )
+        t
     }
 }
